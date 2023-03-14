c9c8a92b94848a4a12607c056be532b4c18e70cc
Denis Sirotkin <sirotkin@denis-02.ru>
2023-03-14 23:16:01 +0300

    Add timetable-mirea

---
+++ web-site/timetable-mirea/src/components/header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    return(
+    <header>
+        <header className="header-left">
+            <p className="main_header">Главная</p>
+            <p className="timetable_header">Расписание</p>
+        </header>
+    </header>
+    );
+}
+
+export default Header;
