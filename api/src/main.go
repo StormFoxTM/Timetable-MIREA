d6456545ab493fcdfa74fbe70a49cb774a1b18d7
Denis Sirotkin <sirotkin@denis-02.ru>
2023-03-14 19:16:45 +0300

    Added comments in the code

--- api/src/main.go
+++ api/src/main.go
@@ -1,126 +1,153 @@
 package main
 
+// Импортируем необходимые модули и библиотеки
 import (
 	"encoding/json"
-	"io/ioutil"
-	"log"
 	"net/http"
 	"strconv"
 	"strings"
 
-	"knocker/pgsql"
+	"knocker/pgsql" // Импортируем модуль pgsql из нашего собственного пакета knocker
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5"
 )
 
+// Основная функция main
 func main() {
+	// Создаем новый роутер Gin с настройками по умолчанию
 	router := gin.Default()
-	router.GET("/api/timetable", func(context *gin.Context) {
-		get_function(context)
-	})
-	router.POST("/api/timetable", func(context *gin.Context) {
-		post_function(context)
-	})
+
+	// Добавляем обработчик GET-запроса по пути "/api/timetable"
+	router.GET("/api/timetable", getFunction)
+
+	// Запускаем сервер Gin на порту 9888
 	err := router.Run(":9888")
 	if err != nil {
+		// В случае ошибки выводим сообщение об ошибке и завершаем программу
 		panic("[Error] failed to start Gin server due to: " + err.Error())
 	}
 }
 
-func get_function(context *gin.Context) {
+func getFunction(context *gin.Context) {
+	// Получаем параметры запроса
 	data := context.Request.URL.Query()
+
+	// Получаем количество параметров "group", "lecture" и "auditorium"
 	group := len(data["group"])
-	lectur := len(data["lectur"])
+	lecture := len(data["lecture"])
 	auditorium := len(data["auditorium"])
 
+	// В зависимости от того, сколько параметров указано, выбираем соответствующую функцию
 	switch 1 {
 	case group:
+		// Получаем данные по группе и кодируем их в JSON
 		dataGroup, err := get_group(context)
 		dataGroupJSON, err_json := json.Marshal(dataGroup)
+
+		// Если не возникло ошибок, возвращаем данные по группе в формате JSON
 		if err == nil && err_json == nil {
 			context.String(http.StatusOK, string(dataGroupJSON))
 		} else if err == pgx.ErrNoRows {
+			// Если нет данных, возвращаем ошибку "По запросу ничего не найдено"
 			context.String(http.StatusBadRequest, "По запросу ничего не найдено")
 		} else {
+			// Если возникли ошибки, возвращаем ошибку "Ошибка в запросе!"
 			context.String(http.StatusBadRequest, "Ошибка в запросе!")
 		}
 
-	case lectur:
+	case lecture:
+		// Получаем данные по лектору и кодируем их в JSON
 		dataLectur, err := get_lecturer(context)
 		dataJSON, err_json := json.Marshal(dataLectur)
+
+		// Если не возникло ошибок, возвращаем данные по лектору в формате JSON
 		if err == nil && err_json == nil {
 			context.String(http.StatusOK, string(dataJSON))
 		} else if err == pgx.ErrNoRows {
+			// Если нет данных, возвращаем ошибку "По запросу ничего не найдено"
 			context.String(http.StatusBadRequest, "По запросу ничего не найдено")
 		} else {
+			// Если возникли ошибки, возвращаем ошибку "Ошибка в запросе!"
 			context.String(http.StatusBadRequest, "Ошибка в запросе!")
 		}
 
 	case auditorium:
+		// Получаем данные по аудитории и кодируем их в JSON
 		dataAuditorium, err := get_auditorium(context)
 		dataJSON, err_json := json.Marshal(dataAuditorium)
+
+		// Если не возникло ошибок, возвращаем данные по аудитории в формате JSON
 		if err == nil && err_json == nil {
 			context.String(http.StatusOK, string(dataJSON))
 		} else if err == pgx.ErrNoRows {
+			// Если нет данных, возвращаем ошибку "По запросу ничего не найдено"
 			context.String(http.StatusBadRequest, "По запросу ничего не найдено")
 		} else {
+			// Если возникли ошибки, возвращаем ошибку "Ошибка в запросе!"
 			context.String(http.StatusBadRequest, "Ошибка в запросе!")
 		}
 
+	// Если количество параметров не соответствует ни одному из вариантов, возвращаем ошибку "Ошибка в запросе!"
 	default:
 		context.String(http.StatusBadRequest, "Ошибка в запросе!")
 	}
-
 }
 
+// get_group function: получает группу и запрашивает ее расписание из базы данных по заданным дням и неделе.
 func get_group(context *gin.Context) (pgsql.DataGroupRequests, error) {
+	// Получаем параметры запроса из URL.
 	data := context.Request.URL.Query()
+	// Получаем номер текущей недели и дня недели.
 	week_int, day_int := getWeekAndDay(context)
+	// Получаем расписание из базы данных на основе параметров запроса и текущей даты.
 	return pgsql.GetTimetableGroup(strings.ToUpper(data["group"][0]), week_int, day_int)
 }
 
+// get_lecturer function: получает преподавателя и запрашивает его расписание из базы данных по заданным дням и неделе.
 func get_lecturer(context *gin.Context) (pgsql.DataLecturRequests, error) {
+	// Получаем параметры запроса из URL.
 	data := context.Request.URL.Query()
+	// Получаем номер текущей недели и дня недели.
 	week_int, day_int := getWeekAndDay(context)
-	return pgsql.GetTimetableLectur(strings.Title(data["lectur"][0]), week_int, day_int)
+	// Получаем расписание из базы данных на основе параметров запроса и текущей даты.
+	return pgsql.GetTimetableLectur(strings.Title(data["lecture"][0]), week_int, day_int)
 }
 
+// get_auditorium function: получает аудиторию и запрашивает ее расписание из базы данных по заданным дням и неделе.
 func get_auditorium(context *gin.Context) (pgsql.DataAuditoriumRequests, error) {
+	// Получаем параметры запроса из URL.
 	data := context.Request.URL.Query()
+	// Получаем номер текущей недели и дня недели.
 	week_int, day_int := getWeekAndDay(context)
+	// Получаем расписание из базы данных на основе параметров запроса и текущей даты.
 	return pgsql.GetTimetableAuditorium(strings.ToUpper(data["auditorium"][0]), week_int, day_int)
 }
 
+// Функция получает контекст запроса и возвращает номер недели и дня в числовом формате
 func getWeekAndDay(context *gin.Context) (int, int) {
+	// Извлекаем данные из URL-запроса
 	data := context.Request.URL.Query()
 	week, day := "", ""
+	// Если параметры "week" и "day" переданы в запросе, сохраняем их
 	if len(data["week"]) > 0 {
 		week = data["week"][0]
 	}
 	if len(data["day"]) > 0 {
 		day = data["day"][0]
 	}
+
+	// Преобразуем строковые значения в числа
 	week_int, _ := strconv.Atoi(week)
 	day_int, _ := strconv.Atoi(day)
-	return week_int, day_int
-}
 
-func post_function(context *gin.Context) {
-	jsonData, err := ioutil.ReadAll(context.Request.Body)
-	if err != nil {
-		// Handle error
+	// Если номер недели или дня не входят в допустимый диапазон, устанавливаем значение 0
+	if week_int > 2 || week_int < 1 {
+		week_int = 0
 	}
-	data := string(jsonData)
-	log.Println(data)
-
-	// group := len(data["group"])
-	// lecturer := len(data["lecturer"])
-	// auditorium := len(data["auditorium"])
-	// subject := len(data["subject"])
-	// number := len(data["number"])
-	// type_of_subject := len(data["type_of_subject"])
-	// log.Println(group, subject, lecturer, auditorium, number, type_of_subject)
-
-	context.String(http.StatusOK, "Принял")
+	if day_int > 7 || day_int < 1 {
+		day_int = 0
+	}
+	// Возвращаем значения в числовом формате
+	return week_int, day_int
 }
--- api/src/pgsql/pgsql.go
+++ api/src/pgsql/pgsql.go
@@ -2,38 +2,43 @@ package pgsql
 
 import (
 	"context"
-	"log"
 	"strings"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+// тип для работы с таблицей преподавателей
 type lecturer struct {
 	id_lecturer int
 	full_name   string
 }
 
+// тип для работы с таблицей расписания звонков
 type call_schedule struct {
 	subject_number int
 	time_start     time.Time
 	time_end       time.Time
 }
 
+// тип для работы с таблицей курсов
 type course struct {
 	id_of_course int
 }
 
+// тип для работы с таблицей степеней
 type degree struct {
 	id_of_degree    int
 	degree_of_study string
 }
 
+// тип для работы с таблицей институтов
 type institute struct {
 	id_of_the_institute   int
 	name_of_the_institute string
 }
 
+// тип для работы с таблицей учебных групп
 type study_group struct {
 	id_group     int
 	group_name   string
@@ -42,6 +47,7 @@ type study_group struct {
 	id_degree    int
 }
 
+// тип для работы с таблицей расписания занятий
 type timetable struct {
 	id_to_group       int
 	subject_to_number int
@@ -52,14 +58,17 @@ type timetable struct {
 	type_of_week      int
 }
 
+// тип данных для ответа на запросы по группам
 type DataGroupRequests struct {
 	Weeks []DataForWeekGroup `json:"weeks"`
 }
 
+// тип данных для ответа на запросы по группам за конкретную неделю
 type DataForWeekGroup struct {
 	Day []DataForDayGroup `json:"day"`
 }
 
+// тип данных для ответа на запросы по группам за конкретный день
 type DataForDayGroup struct {
 	Subject_to_number int    `json:"subject_to_number"`
 	Subject_title     string `json:"subject_title"`
@@ -67,14 +76,17 @@ type DataForDayGroup struct {
 	Auditorium        string `json:"auditorium"`
 }
 
+// тип данных для ответа на запросы по преподавателям
 type DataLecturRequests struct {
 	Weeks []DataForWeekLectur `json:"weeks"`
 }
 
+// тип данных для ответа на запросы по преподавателям за конкретную неделю
 type DataForWeekLectur struct {
 	Day []DataForDayLectur `json:"day"`
 }
 
+// тип данных для ответа на запросы по преподавателям за конкретный день
 type DataForDayLectur struct {
 	Subject_to_number int    `json:"subject_to_number"`
 	Name_group        string `json:"name_group"`
@@ -82,14 +94,17 @@ type DataForDayLectur struct {
 	Auditorium        string `json:"auditorium"`
 }
 
+// тип данных для ответа на запросы по аудиториям
 type DataAuditoriumRequests struct {
 	Weeks []DataForWeekAuditorium `json:"weeks"`
 }
 
+// тип данных для ответа на запросы по аудиториям за конкретную неделю
 type DataForWeekAuditorium struct {
 	Day []DataForDayAuditorium `json:"day"`
 }
 
+// тип данных для ответа на запросы по аудиториям за конкретный день
 type DataForDayAuditorium struct {
 	Subject_to_number int    `json:"subject_to_number"`
 	Name_group        string `json:"name_group"`
@@ -97,8 +112,11 @@ type DataForDayAuditorium struct {
 	Name_lectur       string `json:"name_lectur"`
 }
 
+// Функция подключения к БД PostgreSQL
 func connectToDB() (*pgxpool.Pool, error) {
+	// Строка подключения к БД
 	urlDB := "postgres://admin:admin@postgres:5432/TimeTableDB"
+	// Подключение к БД
 	db, err := pgxpool.New(context.Background(), urlDB)
 	if err != nil {
 		return nil, err
@@ -106,9 +124,14 @@ func connectToDB() (*pgxpool.Pool, error) {
 	return db, nil
 }
 
+// Функция GetTimetableGroup возвращает расписание занятий для группы по названию group на указанный тип недели и день недели.
+// type_week - тип недели (1 - нечётная, 2 - чётная, 0 - все недели)
+// day_week - день недели (1 - понедельник, 2 - вторник, ..., 7 - воскресенье, 0 - все дни)
 func GetTimetableGroup(group string, type_week int, day_week int) (DataGroupRequests, error) {
 	var dataGroupRequests DataGroupRequests
+	// Подключение к БД
 	db, err := connectToDB()
+	// Получение ID группы
 	id_group, group_err := GetGroupID(group)
 
 	start_type_of_week := type_week
@@ -116,13 +139,14 @@ func GetTimetableGroup(group string, type_week int, day_week int) (DataGroupRequ
 
 	start_day_of_week := day_week
 	end_day_of_week := day_week
+	// Обработка случаев, когда тип недели или день недели не указаны
 	if type_week == 0 {
 		start_type_of_week = 1
 		end_type_of_week = 2
 	}
 	if day_week == 0 {
 		start_day_of_week = 1
-		end_day_of_week = 6
+		end_day_of_week = 7
 	}
 
 	if err == nil && group_err == nil {
@@ -135,6 +159,7 @@ func GetTimetableGroup(group string, type_week int, day_week int) (DataGroupRequ
 				var dataForWeek DataForWeekGroup
 				for subject_to_number := 1; subject_to_number <= 8; subject_to_number += 1 {
 					var TimeTable timetable
+					// Запрос на получение расписания для указанной группы, номера пары, дня недели и типа недели
 					err = db.QueryRow(context.Background(),
 						"SELECT * FROM timetable WHERE id_to_group=$1 AND subject_to_number=$2 AND day_week=$3 AND type_of_week=$4;",
 						id_group, subject_to_number, day_of_week, type_of_week).Scan(&TimeTable.id_to_group, &TimeTable.subject_to_number,
@@ -155,6 +180,7 @@ func GetTimetableGroup(group string, type_week int, day_week int) (DataGroupRequ
 				dataGroupRequests.Weeks = append(dataGroupRequests.Weeks, dataForWeek)
 			}
 		}
+		// Если количество ошибок не равно количеству запросов, то вернуть nil вместо ошибки
 		if count_err != count {
 			err = nil
 		}
@@ -162,35 +188,45 @@ func GetTimetableGroup(group string, type_week int, day_week int) (DataGroupRequ
 	return dataGroupRequests, err
 }
 
+// Функция GetTimetableLectur возвращает расписание занятий для преподавателя по имени lecturer на указанный тип недели и день недели.
+// type_week - тип недели (1 - нечётная, 2 - чётная, 0 - все недели)
+// day_week - день недели (1 - понедельник, 2 - вторник, ..., 7 - воскресенье, 0 - все дни)
 func GetTimetableLectur(lecturer string, type_week int, day_week int) (DataLecturRequests, error) {
 	var dataRequests DataLecturRequests
+	// Подключение к БД
 	db, err := connectToDB()
+	// Получение id преподавателя
 	id_lectur, lectur_err := GetLecturerID(lecturer)
 
+	// Определение типа и диапазона недель, если заданы все недели или все дни
 	start_type_of_week := type_week
 	end_type_of_week := type_week
-
 	start_day_of_week := day_week
 	end_day_of_week := day_week
+
+	// Обработка случаев, когда тип недели или день недели не указаны
 	if type_week == 0 {
 		start_type_of_week = 1
 		end_type_of_week = 2
 	}
 	if day_week == 0 {
 		start_day_of_week = 1
-		end_day_of_week = 6
+		end_day_of_week = 7
 	}
-	log.Println(lecturer)
+
 	if err == nil && lectur_err == nil {
 		defer db.Close()
-		count := 0
-		count_err := 0
+		count := 0     // счетчик занятий
+		count_err := 0 // счетчик ошибок
 
+		// Обход всех недель и дней
 		for type_of_week := start_type_of_week; type_of_week <= end_type_of_week; type_of_week += 1 {
 			for day_of_week := start_day_of_week; day_of_week <= end_day_of_week; day_of_week += 1 {
 				var dataForWeek DataForWeekLectur
+				// Обход всех пар на заданный день и тип недели
 				for subject_to_number := 1; subject_to_number <= 8; subject_to_number += 1 {
 					var TimeTable timetable
+					// Выборка занятий по id преподавателя, номеру пары, дню недели и типу недели
 					err = db.QueryRow(context.Background(),
 						"SELECT * FROM timetable WHERE id_lectur=$1 AND subject_to_number=$2 AND day_week=$3 AND type_of_week=$4;",
 						id_lectur, subject_to_number, day_of_week, type_of_week).Scan(&TimeTable.id_to_group, &TimeTable.subject_to_number,
@@ -199,6 +235,7 @@ func GetTimetableLectur(lecturer string, type_week int, day_week int) (DataLectu
 						count_err += 1
 					} else {
 						var dataForDay DataForDayLectur
+						// Заполнение данных для одной пары
 						dataForDay.Auditorium = strings.TrimSpace(TimeTable.auditorium)
 						dataForDay.Subject_title = strings.TrimSpace(TimeTable.subject_title)
 						dataForDay.Subject_to_number = TimeTable.subject_to_number
@@ -210,6 +247,7 @@ func GetTimetableLectur(lecturer string, type_week int, day_week int) (DataLectu
 				dataRequests.Weeks = append(dataRequests.Weeks, dataForWeek)
 			}
 		}
+		// Если количество ошибок не равно количеству запросов, то вернуть nil вместо ошибки
 		if count_err != count {
 			err = nil
 		}
@@ -217,22 +255,29 @@ func GetTimetableLectur(lecturer string, type_week int, day_week int) (DataLectu
 	return dataRequests, err
 }
 
+// Функция GetTimetableAuditorium возвращает расписание занятий для преподавателя по названию auditorium на указанный тип недели и день недели.
+// type_week - тип недели (1 - нечётная, 2 - чётная, 0 - все недели)
+// day_week - день недели (1 - понедельник, 2 - вторник, ..., 7 - воскресенье, 0 - все дни)
 func GetTimetableAuditorium(auditorium string, type_week int, day_week int) (DataAuditoriumRequests, error) {
 	var dataRequests DataAuditoriumRequests
+	// Подключение к БД
 	db, err := connectToDB()
 
+	// Определение типа и диапазона недель, если заданы все недели или все дни
 	start_type_of_week := type_week
 	end_type_of_week := type_week
 
 	start_day_of_week := day_week
 	end_day_of_week := day_week
+
+	// Обработка случаев, когда тип недели или день недели не указаны
 	if type_week == 0 {
 		start_type_of_week = 1
 		end_type_of_week = 2
 	}
 	if day_week == 0 {
 		start_day_of_week = 1
-		end_day_of_week = 6
+		end_day_of_week = 7
 	}
 	if err == nil {
 		defer db.Close()
@@ -252,6 +297,7 @@ func GetTimetableAuditorium(auditorium string, type_week int, day_week int) (Dat
 						count_err += 1
 					} else {
 						var dataForDay DataForDayAuditorium
+						// Заполнение данных для одной пары
 						dataForDay.Name_group, _ = GetGroupName(TimeTable.id_to_group)
 						dataForDay.Subject_title = strings.TrimSpace(TimeTable.subject_title)
 						dataForDay.Subject_to_number = TimeTable.subject_to_number
@@ -263,6 +309,7 @@ func GetTimetableAuditorium(auditorium string, type_week int, day_week int) (Dat
 				dataRequests.Weeks = append(dataRequests.Weeks, dataForWeek)
 			}
 		}
+		// Если количество ошибок не равно количеству запросов, то вернуть nil вместо ошибки
 		if count_err != count {
 			err = nil
 		}
@@ -270,11 +317,14 @@ func GetTimetableAuditorium(auditorium string, type_week int, day_week int) (Dat
 	return dataRequests, err
 }
 
+// Функция получения имени преподавателя по его идентификатору
 func GetLecturerName(lecturer_id int) (string, error) {
+	// Открытие соединения с БД
 	var lecturer_name string
 	db, err := connectToDB()
 	if err == nil {
 		defer db.Close()
+		// Выполнение запроса на выборку имени преподавателя из БД
 		err = db.QueryRow(context.Background(), "SELECT full_name FROM lecturer WHERE id_lecturer=$1;", lecturer_id).Scan(&lecturer_name)
 		if err == nil {
 			return strings.TrimSpace(lecturer_name), nil
@@ -283,11 +333,14 @@ func GetLecturerName(lecturer_id int) (string, error) {
 	return "", err
 }
 
+// Функция получения названия группы по ее идентификатору
 func GetGroupName(group_id int) (string, error) {
+	// Открытие соединения с БД
 	var group_name string
 	db, err := connectToDB()
 	if err == nil {
 		defer db.Close()
+		// Выполнение запроса на выборку названия группы из БД
 		err = db.QueryRow(context.Background(), "SELECT group_name FROM study_group WHERE id_group=$1;", group_id).Scan(&group_name)
 		if err == nil {
 			return strings.TrimSpace(group_name), nil
@@ -296,11 +349,14 @@ func GetGroupName(group_id int) (string, error) {
 	return "", err
 }
 
+// Функция получения идентификатора преподавателя по его имени
 func GetLecturerID(lecturer_name string) (int, error) {
+	// Открытие соединения с БД
 	var id_lecturer int
 	db, err := connectToDB()
 	if err == nil {
 		defer db.Close()
+		// Выполнение запроса на выборку идентификатора преподавателя из БД
 		err = db.QueryRow(context.Background(), "SELECT id_lecturer FROM lecturer WHERE full_name=$1;", lecturer_name).Scan(&id_lecturer)
 		if err == nil {
 			return id_lecturer, nil
@@ -309,12 +365,14 @@ func GetLecturerID(lecturer_name string) (int, error) {
 	return 0, err
 }
 
+// Функция получения идентификатора группы по ее названию
 func GetGroupID(group string) (int, error) {
+	// Открытие соединения с БД
 	db, err := connectToDB()
 	if err == nil {
 		defer db.Close()
 		var group_ID int
-
+		// Выполнение запроса на выборку идентификатора группы из БД
 		err = db.QueryRow(context.Background(), "SELECT id_group FROM study_group WHERE group_name=$1;", group).Scan(&group_ID)
 		if err == nil {
 			return group_ID, nil
@@ -323,11 +381,14 @@ func GetGroupID(group string) (int, error) {
 	return 0, nil
 }
 
+// Функция получения идентификатора института по его названию
 func GetInstituteID(instituteName string) (int, error) {
+	// Открытие соединения с БД
 	db, err := connectToDB()
 	if err == nil {
 		defer db.Close()
 		var id_of_the_institute int
+		// Выполнение запроса на выборку идентификатора института из БД
 		err_institute := db.QueryRow(context.Background(), "SELECT id_of_the_institute FROM institute WHERE name_of_the_institute=$1;",
 			instituteName).Scan(&id_of_the_institute)
 		if err_institute == nil {
@@ -337,6 +398,7 @@ func GetInstituteID(instituteName string) (int, error) {
 	return 0, err
 }
 
+// Функция получает название группы и вырезает из неё 3 символ
 func find_letter(group string) int {
 	var letter rune
 	for i, elem := range group {
@@ -348,6 +410,7 @@ func find_letter(group string) int {
 	return find_id_degree(string(letter))
 }
 
+// Функция получает 3 символ группы, который определяет степень обучения
 func find_id_degree(letter string) int {
 	switch letter {
 	case "Б":
