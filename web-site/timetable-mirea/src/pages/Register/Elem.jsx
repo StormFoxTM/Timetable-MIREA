1bcd050d05bde526ea87ed82b5367705618ffd5c
LisenokOT <t4rarina-o@yandex.ru>
2023-05-15 19:18:35 +0300

    password

--- web-site/timetable-mirea/src/pages/Register/Elem.jsx
+++ web-site/timetable-mirea/src/pages/Register/Elem.jsx
@@ -3,7 +3,7 @@ import r from './Register.module.css';
 const Elem = (props) => {
     return (
         <div className={r.form_reg}>
-        <p className={r.text_form}>{props.name} </p><input className={r.form_input} />
+        <p className={r.text_form}>{props.name} </p><input className={r.form_input} type={props.type}/>
         </div>
     );
 }
